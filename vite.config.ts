8803656e981b415d5cb896b725ee7b8f1d2410f5
DBC-Works <contact.d.b.c@gmail.com>
2025-06-21 18:41:39 +0900

    ci: correct deploy GitHub Pages setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths()],
-  build: {
-    minify: 'terser',
-  },
-  server: {
-    open: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.BASE_PATH ?? '/',
+    plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths()],
+    build: {
+      minify: 'terser',
+    },
+    server: {
+      open: true,
+    },
+  }
 })
